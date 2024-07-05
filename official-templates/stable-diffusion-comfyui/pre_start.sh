fd03dcbe8b30245e6df826e86a3f0b9b5c2aebb7
Madiator2011 <romanrex19@int.pl>
2024-07-05 18:10:55 +0200

    Fix for ComfyUI venv bugs

--- official-templates/stable-diffusion-comfyui/pre_start.sh
+++ official-templates/stable-diffusion-comfyui/pre_start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 export PYTHONUNBUFFERED=1
-source /venv/bin/activate
+echo 'syncing to workspace, please wait'
+rsync -au --remove-source-files /venv/ /workspace/venv/
+echo 'syncing ComfyUI to workspace, please wait'
 rsync -au --remove-source-files /ComfyUI/ /workspace/ComfyUI/
 ln -s /comfy-models/* /workspace/ComfyUI/models/checkpoints/
 
+source /workspace/venv/bin/activate
+
 cd /workspace/ComfyUI
-python main.py --listen --port 3000 &
+exec /workspace/venv/bin/python main.py --listen --port 3000 &
